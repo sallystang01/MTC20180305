a057b173941b8d6f6daa51a8f17935f2adaec451
sallystang01 <hunterduerr@gmail.com>
2018-03-06 18:22:09 -0500

    Upload

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Calculator
+{
+    partial class CalculatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Operator = new System.Windows.Forms.GroupBox();
+            this.rbDivide = new System.Windows.Forms.RadioButton();
+            this.rbMultiply = new System.Windows.Forms.RadioButton();
+            this.rbSubtract = new System.Windows.Forms.RadioButton();
+            this.rbAdd = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.iv1 = new System.Windows.Forms.TextBox();
+            this.iv2 = new System.Windows.Forms.TextBox();
+            this.resultValue = new System.Windows.Forms.TextBox();
+            this.Value1 = new System.Windows.Forms.Label();
+            this.Value2 = new System.Windows.Forms.Label();
+            this.Result = new System.Windows.Forms.Label();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.Error = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.Operator.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Operator
+            // 
+            this.Operator.Controls.Add(this.rbDivide);
+            this.Operator.Controls.Add(this.rbMultiply);
+            this.Operator.Controls.Add(this.rbSubtract);
+            this.Operator.Controls.Add(this.rbAdd);
+            this.Operator.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Operator.Location = new System.Drawing.Point(12, 54);
+            this.Operator.Name = "Operator";
+            this.Operator.Size = new System.Drawing.Size(268, 56);
+            this.Operator.TabIndex = 0;
+            this.Operator.TabStop = false;
+            this.Operator.Text = "Operator";
+            // 
+            // rbDivide
+            // 
+            this.rbDivide.AutoSize = true;
+            this.rbDivide.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbDivide.Location = new System.Drawing.Point(193, 26);
+            this.rbDivide.Name = "rbDivide";
+            this.rbDivide.Size = new System.Drawing.Size(55, 17);
+            this.rbDivide.TabIndex = 3;
+            this.rbDivide.TabStop = true;
+            this.rbDivide.Text = "Divide";
+            this.rbDivide.UseVisualStyleBackColor = true;
+            this.rbDivide.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // rbMultiply
+            // 
+            this.rbMultiply.AutoSize = true;
+            this.rbMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbMultiply.Location = new System.Drawing.Point(127, 26);
+            this.rbMultiply.Name = "rbMultiply";
+            this.rbMultiply.Size = new System.Drawing.Size(60, 17);
+            this.rbMultiply.TabIndex = 2;
+            this.rbMultiply.TabStop = true;
+            this.rbMultiply.Text = "Multiply";
+            this.rbMultiply.UseVisualStyleBackColor = true;
+            this.rbMultiply.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // rbSubtract
+            // 
+            this.rbSubtract.AutoSize = true;
+            this.rbSubtract.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbSubtract.Location = new System.Drawing.Point(56, 26);
+            this.rbSubtract.Name = "rbSubtract";
+            this.rbSubtract.Size = new System.Drawing.Size(65, 17);
+            this.rbSubtract.TabIndex = 1;
+            this.rbSubtract.TabStop = true;
+            this.rbSubtract.Text = "Subtract";
+            this.rbSubtract.UseVisualStyleBackColor = true;
+            this.rbSubtract.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged_1);
+            // 
+            // rbAdd
+            // 
+            this.rbAdd.AutoSize = true;
+            this.rbAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbAdd.Location = new System.Drawing.Point(6, 26);
+            this.rbAdd.Name = "rbAdd";
+            this.rbAdd.Size = new System.Drawing.Size(44, 17);
+            this.rbAdd.TabIndex = 0;
+            this.rbAdd.TabStop = true;
+            this.rbAdd.Text = "Add";
+            this.rbAdd.UseVisualStyleBackColor = true;
+            this.rbAdd.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(82, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Calculator";
+            // 
+            // iv1
+            // 
+            this.iv1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iv1.Location = new System.Drawing.Point(19, 143);
+            this.iv1.Name = "iv1";
+            this.iv1.Size = new System.Drawing.Size(100, 20);
+            this.iv1.TabIndex = 2;
+            // 
+            // iv2
+            // 
+            this.iv2.Location = new System.Drawing.Point(18, 184);
+            this.iv2.Name = "iv2";
+            this.iv2.Size = new System.Drawing.Size(100, 20);
+            this.iv2.TabIndex = 3;
+            // 
+            // resultValue
+            // 
+            this.resultValue.Location = new System.Drawing.Point(18, 241);
+            this.resultValue.Name = "resultValue";
+            this.resultValue.Size = new System.Drawing.Size(100, 20);
+            this.resultValue.TabIndex = 4;
+            // 
+            // Value1
+            // 
+            this.Value1.AutoSize = true;
+            this.Value1.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Value1.Location = new System.Drawing.Point(45, 126);
+            this.Value1.Name = "Value1";
+            this.Value1.Size = new System.Drawing.Size(48, 15);
+            this.Value1.TabIndex = 5;
+            this.Value1.Text = "Value 1";
+            // 
+            // Value2
+            // 
+            this.Value2.AutoSize = true;
+            this.Value2.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Value2.Location = new System.Drawing.Point(45, 167);
+            this.Value2.Name = "Value2";
+            this.Value2.Size = new System.Drawing.Size(48, 15);
+            this.Value2.TabIndex = 6;
+            this.Value2.Text = "Value 2";
+            // 
+            // Result
+            // 
+            this.Result.AutoSize = true;
+            this.Result.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Result.Location = new System.Drawing.Point(45, 223);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(41, 15);
+            this.Result.TabIndex = 7;
+            this.Result.Text = "Result";
+            // 
+            // Calculate
+            // 
+            this.Calculate.Font = new System.Drawing.Font("Modern No. 20", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Calculate.Location = new System.Drawing.Point(143, 143);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(137, 61);
+            this.Calculate.TabIndex = 8;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // Error
+            // 
+            this.Error.AutoSize = true;
+            this.Error.Location = new System.Drawing.Point(136, 225);
+            this.Error.Name = "Error";
+            this.Error.Size = new System.Drawing.Size(0, 13);
+            this.Error.TabIndex = 9;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // CalculatorForm
+            // 
+            this.AcceptButton = this.Calculate;
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.Error);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.Value2);
+            this.Controls.Add(this.Value1);
+            this.Controls.Add(this.resultValue);
+            this.Controls.Add(this.iv2);
+            this.Controls.Add(this.iv1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Operator);
+            this.Name = "CalculatorForm";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Calculator_Load);
+            this.Operator.ResumeLayout(false);
+            this.Operator.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Operator;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rbAdd;
+        private System.Windows.Forms.RadioButton rbDivide;
+        private System.Windows.Forms.RadioButton rbMultiply;
+        private System.Windows.Forms.RadioButton rbSubtract;
+        private System.Windows.Forms.TextBox iv1;
+        private System.Windows.Forms.TextBox iv2;
+        private System.Windows.Forms.TextBox resultValue;
+        private System.Windows.Forms.Label Value1;
+        private System.Windows.Forms.Label Value2;
+        private System.Windows.Forms.Label Result;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.Label Error;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class CalculatorForm : Form
+    {
+        public CalculatorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calculator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            double FirstValue = 0;
+            double SecondValue = 0;
+            bool numeric = true;
+
+            Error.Text = null;
+            
+
+            numeric = (double.TryParse(iv1.Text, out FirstValue) &&
+                        double.TryParse(iv2.Text, out SecondValue));
+
+            if (iv1.Text != null && iv2.Text != null && numeric)
+            {
+                Calculate.BackColor = Color.Green;
+                iv1.BackColor = Color.White;
+                iv2.BackColor = Color.White;
+                if (rbAdd.Checked)
+                {
+                    resultValue.Text = (FirstValue + SecondValue).ToString();
+                }
+                if (rbSubtract.Checked)
+                {
+                    resultValue.Text = (FirstValue - SecondValue).ToString();
+                }
+                if (rbMultiply.Checked)
+                {
+                    resultValue.Text = (FirstValue * SecondValue).ToString();
+                }
+                if (rbDivide.Checked)
+                {
+                    resultValue.Text = (FirstValue / SecondValue).ToString();
+                }
+
+            }
+            else
+            {
+                Error.Text = "**Cannot Calculate Letters**";
+                Calculate.BackColor = Color.Red;
+                Error.ForeColor = Color.Red;
+                iv1.BackColor = Color.Red;
+                iv2.BackColor = Color.Red;
+                
+            }
+                
+                   
+        }
+    }
+}
